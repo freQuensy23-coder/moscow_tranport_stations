5924a1dbb3a285ab70b48dc7c515e1a491d71ddb
frequensy <mametevalex@gmail.com>
2022-01-22 18:58:50 +0300

    Подмена User-agent

--- api.py
+++ api.py
@@ -1,7 +1,9 @@
 import requests as req
 import logging
+from random import randint
 
 log = logging.getLogger("TransAPI")
+headers = {'User-Agent': f'My User Agent 1.1', 'From': 'domain.com'}
 
 
 class TransAPI:
@@ -14,7 +16,7 @@ class TransAPI:
 
     def get_station_info(self, lon, lat) -> dict:
         link = self.get_link(lon, lat)
-        r = self.requester.get(link)
+        r = self.requester.get(link, headers=headers)
         station_data = r.json()
         log.debug(station_data)
         log.info(f"Get information about station {station_data.get('name')}, ID: {station_data.get('id')}")
