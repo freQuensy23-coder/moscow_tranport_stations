8ffbd860d51e8844cb53a161e72a7c7ce9d4511e
frequensy <mametevalex@gmail.com>
2022-01-23 00:00:02 +0300

    Логгирование для сохранения списка всех оставновок

--- load_stops.py
+++ load_stops.py
@@ -1,13 +1,16 @@
+import logging
 from db.db import engine
 from models import Stop
 from station import stops
 from api import TransAPI
 from sqlalchemy.orm import sessionmaker
 from tqdm import tqdm
+from logging import basicConfig
 
 api = TransAPI()
 session = sessionmaker(bind=engine)()
 stops_list = list(stops())
+basicConfig(level=logging.DEBUG, filemode="w", filename="load_stops.log")
 
 for stop in tqdm(stops_list):
     coord = lon, lat = stop["Lon"], stop["Lat"]
