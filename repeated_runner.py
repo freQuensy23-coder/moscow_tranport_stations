b500d3c11701726014232f322e79e7e8f0d77944
freQuensy <mametevalex@gmail.com>
2022-02-05 13:55:51 +0000

    repeated runner for changing  ip after each use and for nightsleep

---
+++ repeated_runner.py
@@ -0,0 +1,20 @@
+import os
+import time
+from datetime import datetime
+
+
+run_script = "/home/frequensy/MosTransParser/moscow_tranport_stations/venv/bin/python3.9 /home/frequensy/MosTransParser/moscow_tranport_stations/main.py --stations  /home/frequensy/MosTransParser/moscow_tranport_stations/trams.csv -d --tor >>  transport.log"
+restart_tor = "sudo service tor reload"
+
+while True:
+    if (datetime.now().hour + 3 )%24 < 5:
+        print("sleep")
+        time.sleep(60*60)
+    try:
+        print(f"Запустился в {datetime.now()}")
+        os.system(run_script)
+    except:
+        print(f"SMTH WRONG {time.now()}")
+    os.system(restart_tor)
+    time.sleep(2*60)
+    
