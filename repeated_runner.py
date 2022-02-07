96a986c63f2c6a06a92aa57f82828dc364e715fa
frequensy <mametevalex@gmail.com>
2022-02-08 01:09:05 +0300

    Refactoring

--- repeated_runner.py
+++ repeated_runner.py
@@ -2,19 +2,17 @@ import os
 import time
 from datetime import datetime
 
-
 run_script = "sudo /home/frequensy/MosTransParser/moscow_tranport_stations/venv/bin/python3.9 /home/frequensy/MosTransParser/moscow_tranport_stations/main.py --stations  /home/frequensy/MosTransParser/moscow_tranport_stations/trams.csv -d --tor"
 restart_tor = "sudo service tor reload"
 
 while True:
-    if (datetime.now().hour + 3 )%24 < 5:
+    if (datetime.now().hour + 3) % 24 < 5:
         print("sleep")
-        time.sleep(60*60)
+        time.sleep(60 * 60)
     try:
-        print(f"Запустился в {datetime.now()}")
+        print(f"Started in {datetime.now()}")
         os.system(run_script)
     except:
-        print(f"SMTH WRONG {time.now()}")
+        print(f"Smth went wrong at {time.time()}")
     os.system(restart_tor)
-    time.sleep(2*60)
-    
+    time.sleep(2 * 60)
