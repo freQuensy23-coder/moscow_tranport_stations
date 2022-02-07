5d3544e7d62839591bba0892bcf7f1f40c7423d9
frequensy <mametevalex@gmail.com>
2022-02-08 01:14:00 +0300

    Обновил repeated_runner.py для работы с прокси

--- repeated_runner.py
+++ repeated_runner.py
@@ -2,7 +2,9 @@ import os
 import time
 from datetime import datetime
 
-run_script = "sudo /home/frequensy/MosTransParser/moscow_tranport_stations/venv/bin/python3.9 /home/frequensy/MosTransParser/moscow_tranport_stations/main.py --stations  /home/frequensy/MosTransParser/moscow_tranport_stations/trams.csv -d --tor"
+run_script = "sudo /home/frequensy/MosTransParser/moscow_tranport_stations/venv/bin/python3.9 " \
+             "/home/frequensy/MosTransParser/moscow_tranport_stations/main.py --stations  " \
+             "/home/frequensy/MosTransParser/moscow_tranport_stations/trams.csv -d --proxy_file proxy.txt "
 restart_tor = "sudo service tor reload"
 
 while True:
