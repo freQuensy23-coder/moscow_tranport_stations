135869c4b26357c725ce6505ef6c1d9743f9d262
freQuensy23-coder <mametevalex@gmail.com>
2022-02-11 01:39:03 +0300

    Логгирование основных моментов в телеграм

--- repeated_runner.py
+++ repeated_runner.py
@@ -2,19 +2,27 @@ import os
 import time
 from datetime import datetime
 
+import telegram_send
+
 run_script = "sudo /home/frequensy/MosTransParser/moscow_tranport_stations/venv/bin/python3.9 " \
              "/home/frequensy/MosTransParser/moscow_tranport_stations/main.py --stations  " \
              "/home/frequensy/MosTransParser/moscow_tranport_stations/trams.csv -d --proxy_file proxy.txt "
 restart_tor = "sudo service tor reload"
 
+
+def telelog(msg: str):
+    """Log to telegram"""
+    print(f"{msg}")
+    telegram_send.send(messages=[msg])
+
+
 while True:
     if (datetime.now().hour + 3) % 24 < 5:
-        print("sleep")
+        telelog("sleep")
         time.sleep(60 * 60)
     try:
-        print(f"Started in {datetime.now()}")
+        telelog(f"Started in {datetime.now()}")
         os.system(run_script)
     except:
-        print(f"Smth went wrong at {time.time()}")
-    os.system(restart_tor)
-    time.sleep(2 * 60)
+        telelog(f"Smth went wrong in {datetime.now()}")
+    time.sleep(25)
