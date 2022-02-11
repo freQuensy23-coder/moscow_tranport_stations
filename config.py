de764643bd5f81d99396e89dab8d69e865680f54
freQuensy23-coder <mametevalex@gmail.com>
2022-02-11 16:35:44 +0300

    обновил файл с прокси в конфигурационном файле

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ EXAMPLE_RESULT_FILE = 'example_result.json'
 NUMBER_OF_STOPS = 11783
 LEVEL = logging.INFO
 TIME_LIMIT = 9 * 60  # 9 min
-PROXIES_FILE = "35proxy.txt"
+PROXIES_FILE = "proxy.txt"
 PATH_TO_DB = "sqlite:///" + os.getcwd() + "/db/forecast.db"
 TOR_RESTART_DELAY: int = 10
 headers = {'sec-ch-ua': 'Not;A Brand";v="95", "Google Chrome";v="95", "Chromium";v="95"', 'Host': 'moscowtransport.app',
