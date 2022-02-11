8f92ee865fb88b5d0dcad4ade1015d614a5d2116
freQuensy23-coder <mametevalex@gmail.com>
2022-02-12 00:19:39 +0300

    Add postgre sql support

--- config.py
+++ config.py
@@ -9,7 +9,8 @@ NUMBER_OF_STOPS = 11783
 LEVEL = logging.INFO
 TIME_LIMIT = 9 * 60  # 9 min
 PROXIES_FILE = "proxy.txt"
-PATH_TO_DB = "sqlite:///" + os.getcwd() + "/db/forecast.db"
+PATH_TO_DB = f"postgres:///{os.getenv('db_login')}:{os.getenv('db_pass')}" \
+             f"@{os.getenv('db_host')}:{os.getenv('db-port')}"
 TOR_RESTART_DELAY: int = 10
 headers = {'sec-ch-ua': 'Not;A Brand";v="95", "Google Chrome";v="95", "Chromium";v="95"', 'Host': 'moscowtransport.app',
            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 '
