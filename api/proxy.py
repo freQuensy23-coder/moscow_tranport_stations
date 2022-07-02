3cea0ecaca10cecd642f82125ba830c731b4c798
artembadmaev <artyom.b2011@gmail.com>
2022-07-03 01:12:09 +0300

    Оптимизация использования прокси, обновление менеджера для работы с Тором

--- api/proxy.py
+++ api/proxy.py
@@ -1,55 +1,59 @@
 import os
 import time
-from random import choice
+from random import shuffle
+from multiprocessing import Queue
 
-from config import TOR_RESTART_DELAY
+from config import TOR_RESTART_DELAY, PROXY_REUSE
 from logging import getLogger
 
 log = getLogger(name="ProxyManagers")
 
 
-class ProxyManager:
-    def __init__(self, proxies: list):
-        self._proxies = proxies
-
-    def get_proxy(self) -> dict:
-        """Method should return dict with available method as keys and proxy data as values. Ex:
-        {'http': f'http://{login}:{pass}@{ip}:{port}',
-         'https': f'https://{login}:{pass}@{ip}:{port}'}
-        """
-        proxy = choice(self._proxies)
-        log.debug(f"{proxy} selected by file proxy manager")
-        return proxy
-
-    @staticmethod
-    def _change_ip(*args):
-        """Method calls if requests from ip is not working."""
-        log.info("Changing IP...")
-        # TODO добавлять IP во временную отлегу
-        pass
-
-
-class FileProxyManager(ProxyManager):
+class FileProxyManager:
+    """
+    Менеджер для проксей из файла
+    """
     def __init__(self, file_name):
-        f = open(file_name, "r")
-        proxies = []
-        for line in f:
-            data = line.strip().split(":")  # TODO сделать читаемее
-            proxies.append({
-                'http': f'http://{data[2]}:{data[3]}@{data[0]}:{data[1]}',
-                'https': f'https://{data[2]}:{data[3]}@{data[0]}:{data[1]}',
-            }
-            )
-        super().__init__(proxies)
-        f.close()
+        """Создаем из списка адресов многопотоковую очередь, чтобы адреса распределялись между потоками"""
+        self.proxies = Queue()
+        self.thread_proxies = {}
+
+        with open(file_name, "r") as f:
+            lines = f.readlines()
+            shuffle(lines)
+
+            for line in lines:
+                data = line.strip().split(":")  # TODO проверить на соответствие формату файла с прокси
+                self.proxies.put({
+                    'http': f'http://{data[2]}:{data[3]}@{data[0]}:{data[1]}',
+                    'https': f'https://{data[2]}:{data[3]}@{data[0]}:{data[1]}',
+                })
+
+    def get_proxy(self, thread_id) -> dict:
+        """Метод раздает прокси для каждого нового запроса и меняет адрес после лимита операций"""
+        if thread_id not in self.thread_proxies.keys():
+            self.thread_proxies[thread_id] = (self.proxies.get(), 1)
+            return self.thread_proxies[thread_id][0]
+        elif self.thread_proxies[thread_id][1] < PROXY_REUSE:
+            self.thread_proxies[thread_id][1] += 1
+            return self.thread_proxies[thread_id][0]
+        else:
+            self.proxies.put(self.thread_proxies[thread_id][0])
+            self.thread_proxies[thread_id] = (self.proxies.get(), 1)
+            return self.thread_proxies[thread_id][0]
+
+    def _change_ip(self, thread_id) -> dict:  # TODO добавить удаление прокси из файла
+        """Метод меняет адрес и удаляет его из очереди, когда тот попадает в бан"""
+        self.thread_proxies[thread_id] = (self.proxies.get(), 1)
+        return self.thread_proxies[thread_id]
 
 
-class TorProxy(ProxyManager):
+class TorProxy:
     def __init__(self):
         pass
 
     @staticmethod
-    def _change_ip(req_tor, *args):
+    def _change_ip(req_tor):
         log.info("Changing IP...")
         req_tor.new_id()
         time.sleep(TOR_RESTART_DELAY)
